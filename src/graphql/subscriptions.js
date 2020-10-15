d7502758d9671d6415b996a1e865b41a58d7594d
anastaszi <anastasba@gmail.com>
2020-10-14 23:47:04 -0700

    adding storage and api

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateItem = /* GraphQL */ `
+  subscription OnCreateItem($owner: String!) {
+    onCreateItem(owner: $owner) {
+      id
+      name
+      file {
+        bucket
+        key
+        region
+      }
+      url
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateItem = /* GraphQL */ `
+  subscription OnUpdateItem($owner: String!) {
+    onUpdateItem(owner: $owner) {
+      id
+      name
+      file {
+        bucket
+        key
+        region
+      }
+      url
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteItem = /* GraphQL */ `
+  subscription OnDeleteItem($owner: String!) {
+    onDeleteItem(owner: $owner) {
+      id
+      name
+      file {
+        bucket
+        key
+        region
+      }
+      url
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
