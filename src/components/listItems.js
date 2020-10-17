e74722656e4bb429f6356d68a1dbb390f2f670bd
anastaszi <anastasba@gmail.com>
2020-10-16 20:39:13 -0700

    adding bootstrap

--- src/components/listItems.js
+++ src/components/listItems.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Storage} from 'aws-amplify';
 import { ReactComponent as Bin } from './bin.svg';
-import { ReactComponent as Download } from './download.svg';
+import { ReactComponent as Cloud } from './download.svg';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 function SingleItem(props) {
@@ -12,22 +14,21 @@ function SingleItem(props) {
     window.location.href = url;
   }
   return (
-    <div key={props.index}>
-      <h2>{props.item.filename}</h2>
-      <p>{props.item.username}</p>
-      <p>{props.item.lastname}</p>
-      <p>{props.item.description}</p>
-      <Button variant="outline-danger" onClick={() => props.deleteItem(props.item)}> <Bin/></Button>
-    <Download onClick={() => download(props.item.filename)} className="bin"/>
-    <Bin/>
-    </div>
+    <Row key={props.index}>
+    <Col>{props.item.filename}</Col>
+    <Col>{props.item.username}</Col>
+    <Col>{props.item.lastname}</Col>
+    <Col>{props.item.description}</Col>
+    <Col><Button onClick={() => download(props.item.filename)}><Cloud/></Button>
+    <Button variant="danger" onClick={() => props.deleteItem(props.item)}><Bin/></Button></Col>
+    </Row>
   )
 }
 
 
 export default function ListItems(props) {
 
-  let items = props.items.map((item, index) => <SingleItem item={item} index={index} deleteItem={(e) => props.deleteItem(e)}/>)
+  let items = props.items.map((item, index) => <SingleItem key={item.id} item={item} index={index} deleteItem={(e) => props.deleteItem(e)}/>)
   return (
     <div>
     {items}
