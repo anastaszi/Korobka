c0f525a7adc982f5799084475fde03667f31a52b
anastaszi <anastasba@gmail.com>
2020-10-22 22:26:33 -0700

    new function layer

--- amplify/backend/function/korobkaTriggerCopyS3/src/index.js
+++ amplify/backend/function/korobkaTriggerCopyS3/src/index.js
@@ -1,4 +1,13 @@
-exports.handler = event => {
+/* Amplify Params - DO NOT EDIT
+	API_KOROBKA_GRAPHQLAPIENDPOINTOUTPUT
+	API_KOROBKA_GRAPHQLAPIIDOUTPUT
+	ENV
+	REGION
+	STORAGE_SHTUKI_BUCKETNAME
+Amplify Params - DO NOT EDIT */const AWS = require('aws-sdk')
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => { /*
   if (event && event.Records && (event.Records[0].eventName === 'MODIFY')) {
     var action = event.Records[0].dynamodb;
     var newRecordName = action.NewImage.filename.S;
@@ -7,14 +16,32 @@ exports.handler = event => {
     if (updatedFileName) {
       var path = action.NewImage.key.S;
       console.log("hello");
-      
+
       return Promise.resolve('File updated?');
 
       //s3 copy to the same path with new name;
       //s3 delete old copy
     }
-    return Promise.resolve('Filename stayed the same!');
-  }
 
-  return Promise.resolve('No edit were made!');
+    return Promise.resolve(action);
+  }*/
+  var s3 = new AWS.S3();
+  const srcBucket = "korobka48d121d45ebe456486a8adf23f6db64405605-master";
+  const srcKey = "02.txt";
+
+   try {
+        const params = {
+            Bucket: "korobka48d121d45ebe456486a8adf23f6db64405605-master",
+            Key: "02.txt"
+        };
+        var origimage = await s3.getObject(params).promise();
+        console.log(origimage);
+        return (origimage)
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+  //return Promise.resolve(AWS.VERSION);
 };
